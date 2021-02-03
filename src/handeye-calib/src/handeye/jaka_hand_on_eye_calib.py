f0f039b40f7a8b209454a5b6e37c9585ac0a1a17
sangxin <sangxin@infore.com>
2021-02-03 19:57:25 +0800

    modify jaka

--- src/handeye-calib/src/handeye/jaka_hand_on_eye_calib.py
+++ src/handeye-calib/src/handeye/jaka_hand_on_eye_calib.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 import rospy
+from geometry_msgs.msg import Pose
+
+
+def jaka_callback(pose):
+    print pose
+
+
+def camera_callback(pose):
+    # print pose
+    pass
 
 
 if __name__ == '__main__':
     rospy.init_node("jaka_hand_on_eye_calib", anonymous=False)
     jaka_pose_topic = rospy.get_param("/jaka_hand_on_eye_calib/jaka_pose_topic")
     camera_pose_topic = rospy.get_param("/jaka_hand_on_eye_calib/camera_pose_topic")
+
     rospy.loginfo("Get topic from param server: jaka_pose_topic:"+str(jaka_pose_topic)+" camera_pose_topic:"+str(camera_pose_topic))
+
+    rospy.Subscriber(jaka_pose_topic, Pose, jaka_callback)
+    rospy.Subscriber(camera_pose_topic, Pose, camera_callback)
+    while not rospy.is_shutdown():
+        command = str(raw_input("input r to record,c to calculate,q to quit:"))
+        if command == "r" :
+            print "record"
+        elif command=='c' :
+            print "calculate"
